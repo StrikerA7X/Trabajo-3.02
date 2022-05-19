634e920efee1bacd90b9237e36a9cdf8bac027d1
TUF GAMING <TUF GAMING@LAPTOP-E7M3AB9F>
2022-05-19 18:19:55 -0500

    Tarea 1

---
+++ src/main/java/ventana/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ventana;
+
+/**
+ *
+ * @author TUF GAMING
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+      Ventana v1 = new Ventana();
+      v1.setVisible(true); //Visible ventana
+    }
+    
+}
+    

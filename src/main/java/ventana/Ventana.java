c89a41ebcd5fd5721d0e8aa40c051f8d2f4c73ae
TUF GAMING <TUF GAMING@LAPTOP-E7M3AB9F>
2022-05-19 18:20:02 -0500

    Tarea 1

---
+++ src/main/java/ventana/Ventana.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ventana;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author TUF GAMING
+ */
+public class Ventana extends JFrame{ 
+    public Ventana(){
+        this.setSize(500, 500); //Tamaño
+    }
+}
